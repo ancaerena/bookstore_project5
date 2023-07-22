47c9bbcc7fc357c159bfe3893708e0d070bd21da
ancaerena <anca.erena@yahoo.ro>
2023-07-22 22:44:55 +0000

    customized admin

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Book, Category
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Category)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'author',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
