6dd759d4c1edc0b5893e2a6bdc4627a27d4acbbc
ancaerena <anca.erena@yahoo.ro>
2023-07-23 01:50:32 +0000

    Add product details

--- books/views.py
+++ books/views.py
@@ -13,3 +13,14 @@ def all_books(request):
     }
 
     return render(request, 'books/books.html', context)
+
+def book_detail(request, book_id):
+    """ A view to show individual book details """
+
+    book = get_object_or_404(Book, sku=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
