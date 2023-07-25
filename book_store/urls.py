bc09b9c8df213c1d7438c4f6ce6272394f25a96c
ancaerena <anca.erena@yahoo.ro>
2023-07-25 22:52:47 +0000

    Create checkout html and install crispy forms

--- book_store/urls.py
+++ book_store/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('books/', include('books.urls')),
     path('book_club/', include('book_club.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
