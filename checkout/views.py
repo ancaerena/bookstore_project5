a5d452fb68ddb304f1022bcf624fba598133daf1
ancaerena <anca.erena@yahoo.ro>
2023-07-25 23:16:25 +0000

    Add stripe js links and js files

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NXuAKCssaSqNdf8JWCokRhB9VzCRQyXVoW16DLHB6wvcEJZpSf4gIBAUuyVPOyRIXyH5byISOnN1k7EDcE56eom00pHR7Mgw2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
