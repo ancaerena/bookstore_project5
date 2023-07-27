6794b7d0aab8f3d57fb29bab420130e9eedf7967
ancaerena <anca.erena@yahoo.ro>
2023-07-27 21:50:27 +0000

    Create add book view, url and html

--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Book, Category
+from .forms import BookForm
 
 # Create your views here.
 
@@ -65,3 +68,13 @@ def book_detail(request, book_id):
     }
 
     return render(request, 'books/book_detail.html', context)
+
+def add_book(request):
+    """ Add a product to the store """
+    form = BookForm()
+    template = 'books/add_book.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
