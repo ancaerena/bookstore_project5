4d7b6204fab135a6a129cb11b1d4d1e45f86fdce
ancaerena <anca.erena@yahoo.ro>
2023-07-29 02:51:09 +0000

    Deployment part 2

--- book_club/views.py
+++ book_club/views.py
@@ -6,18 +6,11 @@ from .forms import *
 def book_club(request):
     subjects = Subject.objects.all()
     count = subjects.count()
-    # comments =[]
+    
     commentlist = Comments.objects.all()
     print(commentlist)
     print(subjects)
-    # print(subjects[1])
-    # print(dir(comm[1]))
-    # print(comm[1].discuss)
-    # for i in subjects:
-        # comments.append(i.comments_set.all())
-        # print(i.comments_set)
-        # print(dir(i.comments_set.all()))
-        # print(i.comments_set.all().values)
+
  
     context={'subjects':subjects,
               'count':count,
--- book_store/settings.py
+++ book_store/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure--5k6hh^@-cr@n8r54i^k+^o%z5c5fp0-x8j)&qmy$h9$jk68*s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ancaerena-bookstoreproj-jsmwz9fayd7.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['thebookshelfchair-c10ecc9e636c.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +122,18 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
