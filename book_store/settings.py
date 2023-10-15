b2fe0ce76acd63c5305ec7f3c347521f44b478ae
ancaerena <anca.erena@yahoo.ro>
2023-10-15 15:12:29 +0000

    Add env file and import it in settings

--- book_store/settings.py
+++ book_store/settings.py
@@ -13,6 +13,9 @@ import os
 
 import dj_database_url
 
+if os.path.exists("env.py"):
+    import env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
